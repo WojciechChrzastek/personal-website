f00a3a4602a5046a2f26af0b5c9cb4ceba3fea8e
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-05-15 10:49:52 +0200

    Change server-side form validation and submit form using ajax

--- send.php
+++ send.php
@@ -8,69 +8,62 @@ use PHPMailer\ PHPMailer\ PHPMailer;
 use PHPMailer\ PHPMailer\ SMTP;
 use PHPMailer\ PHPMailer\ Exception;
 
-    $name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
-    $email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
-    $message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
+$name = filter_var($_POST['name'], FILTER_SANITIZE_STRING);
+$email = filter_var($_POST['email'], FILTER_SANITIZE_STRING);
+$message = filter_var($_POST['message'], FILTER_SANITIZE_STRING);
 
-    //fields validation
-    if (empty(['name'])) {
-        header("location: http://chrzastek.net/#contact");
+if($_POST['honey'] === "" && $name !== "" && $email !== "" && $message !== ""){
+	if(!filter_var($email, FILTER_VALIDATE_EMAIL)) 
+	{
+	    $signal = 'bad';
+        $msg = 'Please type in valid email address.';
     }
-
-    if (empty(['email'])) {
-        header("location: http://chrzastek.net/#contact");
-    }
-
-    if (empty(['message'])) {
-        header("location: http://chrzastek.net/#contact");
-    }
-
-    //honeypot validation
-if (empty($_POST['honey'])) {
-
-    $mail = new PHPMailer(true);
-    $mail -> CharSet = "UTF-8";
-
-    try {
-
-        //Server settings
-        $mail->SMTPDebug = 2;
-        //$mail->isSMTP();
+    else{
+        $mail = new PHPMailer();
+        $mail -> CharSet = "UTF-8";
+        // $mail -> SMTPDebug = 2;
+        $mail->isSMTP();
         $mail -> Host = "smtp.abc.com";
         $mail -> SMTPAuth = "true";
         $mail -> Username = "mail@abc.com";
         $mail -> Password = "12345";
         $mail -> SMTPSecure = "tls";
         $mail -> Port = "587";
-
+        
         //User data
         $mail -> From = $email;
         $mail -> FromName = $name;
         $mail -> addReplyTo = $email;
-
+        
         //Recipent
         $mail -> addAddress("w.chrzastek@gmail.com");
-
+        
         //Message body
-        $body = "<p><strong>Hello</strong>, you have recieved an enquiry from ".$name.
-        ". The message is: ".$message.
-        "</p>";
-
+        $body = "<p>" . "The message from <strong>".$name. "</strong> is: <br><br>" .$message. "</p>";
+        
         //Content
         $mail -> isHTML(true);
-        $mail -> Subject = "Your website contact form enquiry from: ".$name;
+        $mail -> Subject = "Chrzastek.net enquiry from: ".$name;
         $mail -> Body = "$body";
         $mail -> AltBody = "strip_tags($body)";
-
-        $mail -> send();
-        // echo "Message has been sent!";
-        header("location: http://chrzastek.net/#contact");
-
-    } catch (Exception $e) {
-        echo "Message could not be sent!";
-        echo "Mailer error: ".$mail -> ErrorInfo;
+        
+        if(!$mail->send()) {
+            echo nl2br ("Message could not be sent.\n");
+            echo 'Mailer Error: ' . $mail -> ErrorInfo;
+        } else {
+            $signal = 'ok';
+            $msg = 'Your message has been sent.';
+        }
     }
-
 }
+else{
+    $signal = 'bad';
+    $msg = 'Please fill in all the fields.';
+}
+$data = array(
+    'signal' => $signal,
+    'msg' => $msg
+);
+echo json_encode($data);
 
 ?>
