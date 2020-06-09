649a887e9ad59735d4b62b2a09183ffc44bfd98e
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-09 13:54:50 +0200

    Implement shrink-on-scroll navbar

--- js/main.js
+++ js/main.js
@@ -1,3 +1,32 @@
+//-----NAVBAR-SHNINK-ON-SCROLL-----
+$(document).ready(function () {
+  var toggleAffix = function (affixElement, scrollElement, wrapper) {
+    var height = affixElement.outerHeight(),
+      top = wrapper.offset().top;
+
+    if (scrollElement.scrollTop() >= top) {
+      wrapper.height(height);
+      affixElement.addClass("affix");
+    } else {
+      affixElement.removeClass("affix");
+      wrapper.height("auto");
+    }
+  };
+
+  $('[data-toggle="affix"]').each(function () {
+    var ele = $(this),
+      wrapper = $("<div></div>");
+
+    ele.before(wrapper);
+    $(window).on("scroll resize", function () {
+      toggleAffix(ele, $(this), wrapper);
+    });
+
+    // init
+    toggleAffix(ele, $(window), wrapper);
+  });
+});
+
 //-----NAVBAR-COLLAPSE-----
 $(".navbar-nav>a").on("click", function () {
   $(".navbar-collapse").collapse("hide");
