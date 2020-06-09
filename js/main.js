09e3c7e4007dfa4229c42bfdc85fbd844bd42558
Wojciech Chrzastek <w.chrzastek@gmail.com>
2020-06-09 21:24:15 +0200

    Reformat code styling. Correct some typo errors.

--- js/main.js
+++ js/main.js
@@ -3,7 +3,6 @@ $(document).ready(function () {
   var toggleAffix = function (affixElement, scrollElement, wrapper) {
     var height = affixElement.outerHeight(),
       top = wrapper.offset().top;
-
     if (scrollElement.scrollTop() >= top) {
       wrapper.height(height);
       affixElement.addClass("affix");
@@ -12,16 +11,13 @@ $(document).ready(function () {
       wrapper.height("auto");
     }
   };
-
   $('[data-toggle="affix"]').each(function () {
     var ele = $(this),
       wrapper = $("<div></div>");
-
     ele.before(wrapper);
     $(window).on("scroll resize", function () {
       toggleAffix(ele, $(this), wrapper);
     });
-
     // init
     toggleAffix(ele, $(window), wrapper);
   });
